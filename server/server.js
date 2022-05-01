22395cad17ff72b2d241d993d986c30e2c6d43ec
ZhouSusan <szhou089@gmail.com>
2022-05-01 03:03:54 -0700

    set event emitters and event listeners on server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('Sucess!'));
 
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const server = app.listen(8000, () =>
+    console.log('The server is all fired up on port 8000')
+);
 
 const io = require('socket.io')(server, { cors: true });
+
+io.on("connection", socket => {
+    socket.on("event_from_client", data => {
+        socket.broadcast.emit("send_data_to_all_other_clients", data);
+    })
+})
